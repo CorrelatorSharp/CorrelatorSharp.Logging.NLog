e76b63c43e032f6eca7bb22bde6f3840418fdef1
Ivan Zlatev <ivan@ivanz.com>
2016-02-24 21:31:01 +0000

    Code check-in

---
+++ CorrelatorSharp.Logging.NLog/LoggingConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using CorrelatorSharp.Logging.NLog;
+
+namespace CorrelatorSharp.Logging
+{
+    public static class LoggingConfigurationExtensions
+    {
+        public static LoggingConfiguration UseNLog(this LoggingConfiguration config)
+        {
+            config.WithLogManager(new LogManagerAdaptor());
+            return config;
+        }
+    }
+}
