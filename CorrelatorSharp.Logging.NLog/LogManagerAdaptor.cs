966625a872d0d6343f25f12fb8be94b313184e36
Ivan Zlatev <ivan@ivanz.com>
2016-03-03 21:34:06 +0000

    1.2 with update to match 1.2 of CorrelatorSharp.Logging.

--- CorrelatorSharp.Logging.NLog/LogManagerAdaptor.cs
+++ CorrelatorSharp.Logging.NLog/LogManagerAdaptor.cs
@@ -5,11 +5,6 @@ namespace CorrelatorSharp.Logging.NLog
 {
     public class LogManagerAdaptor : ILogManagerAdaptor
     {
-        public ILogger GetCurrentClassLogger()
-        {
-            return new LoggerAdaptor(NLogLogManager.GetCurrentClassLogger());
-        }
-
         public ILogger GetLogger(string name)
         {
             return new LoggerAdaptor(NLogLogManager.GetLogger(name));
